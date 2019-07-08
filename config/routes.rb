9ea85b61ea99e1566b7ca4350014616c4d13f98a
github username <github email address>
2019-07-08 13:04:10 -0600

    Fixed route back to before.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :bros
   resources :scooters
-  root "/bros", to: 'bros#index' 
+  root 'bros#index' 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
